b08e5281baad8f918dd4acc9cb0cd02747409e2a
moriyoshiko <moriyoshiko@morinoMacBook.local>
2019-02-20 13:16:23 +0900

    changed_to_erb_from_haml

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ChatSpace
+module ChatSpace2
   class Application < Rails::Application
-  	    config.generators do |g|
+  	config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
