f3a19cbdf9f845a6365c64cfd01995ac38013823
moriyoshiko <moriyoshiko@morinoMacBook.local>
2019-03-09 12:49:42 +0900

    revised2_for_incremental-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
-    respond_to do |format| 
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id:current_user.id)
+    respond_to do |format|
       format.html
       format.json
     end
@@ -21,8 +21,11 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
-end
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
+
+ end
+
+
 
