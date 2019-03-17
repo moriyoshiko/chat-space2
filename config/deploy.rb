baabf8b67876cc107fb799225f03be8ce6c4332f
moriyoshiko <moriyoshiko@morinoMacBook.local>
2019-03-18 03:54:35 +0900

    ..

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid for current version and patch releases of Capistrano
-# lock '~> 3.11.0'
-lock '3.11.0'
+lock '~> 3.11.0'
+# lock '3.11.0'
 
 set :application, 'chat-space2'
 set :repo_url, 'git@github.com:moriyoshiko/chat-space2.git'
